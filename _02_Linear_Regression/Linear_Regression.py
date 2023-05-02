beb5b0fd858b7b3f6e5868b47c844c1089ce04e6
= <=>
2023-05-02 22:53:20 +0800

    13

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -11,11 +11,9 @@ except ImportError as e:
 
 def ridge(data):
     x_train, y_train = read_data()
-
-    # 添加偏置列
     x_train = np.column_stack((np.ones(len(x_train)), x_train))[:,1:]
 
-    alpha = 1e-9
+    alpha = -500
     # 计算正则化系数矩阵
     l = alpha * np.eye(x_train.shape[1])
 
@@ -26,9 +24,9 @@ def ridge(data):
 def lasso(data):
     x,y=read_data()
     m, n = x.shape
-    max_iter=10000
-    lamda=0.1
-    alpha=1e-9
+    max_iter=1000000
+    lamda=1e-32
+    alpha=1e-7
     theta = np.zeros(n)
 
     for i in range(max_iter):
@@ -36,10 +34,8 @@ def lasso(data):
         # 计算偏导数
         diff = np.zeros(n)
         for j in range(n):
-            if theta[j] > 0:
-                diff[j] = ((1 / m) * np.dot(x[:, j].T, (h_theta - y))) + (lamda / (2 * m))
-            elif theta[j] < 0:
-                diff[j] = ((1 / m) * np.dot(x[:, j].T, (h_theta - y))) - (lamda / (2 * m))
+            if theta[j] != 0:
+                diff[j] = (1 / m) * np.dot(x[:, j].T, (h_theta - y)) + abs(lamda / (2 * m))
             else:
                 diff[j] = np.random.uniform(-lamda / (2 * m), lamda / (2 * m))
         # 更新参数
